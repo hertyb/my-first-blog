76f2689f04a60ccfc0c7b657571c8f179a7dad55
hertyb <hertyb123456@gmail.com>
2017-08-05 16:20:54 +1000

    Changed the HTML for the site.

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Post
 
-class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'created_date']
+#class PostAdmin(admin.ModelAdmin):
+#    list_display = ['title', 'created_date']
 
-admin.site.register(Post, PostAdmin)
+#admin.site.register(Post, PostAdmin)
+
+admin.site.register(Post)
 
 # Register your models here.
